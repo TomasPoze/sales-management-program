492ab62dbd44f457901b697b9ac3887f46fca636
Tomas <tomaspoze2000@gmail.com>
2020-09-02 20:29:57 +0300

    CA-101: Project creation

---
+++ smp-api/src/test/java/com/codeacademy/praktika/PraktikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeacademy.praktika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
